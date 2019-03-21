a93eeada8afe4016412cc12d1652739a6cb27878
Brian Midei <brian.midei@gmail.com>
2019-03-21 00:56:00 -0400

    Initial construction of Spacy Pipeline completed
    Created a pipeline class that can process all files in a given directory,
    pass them to the Spacy pipeline, and store a list of Document objects.
    
    Need to integrate with a tf-record generator.

--- preprocess.py
+++ preprocess.py
@@ -1,21 +1,40 @@
-class Document(object):
-    
-    def __init__(self, rawText):
-        # Initialize with raw text
-        self.raw = rawText
+from __future__ import unicode_literals
+#from spacyOps import Collection
+from pathlib import Path
+import spacy
+import time
+nlp = spacy.load('en')
+nlp.remove_pipe('ner')
+nlp.remove_pipe('tagger')
+nlp.remove_pipe('parser')
+#tokenizer = nlp.create_pipe('tokenizer')
+sentencizer = nlp.create_pipe('sentencizer')
+#nlp.add_pipe(tokenizer)
+nlp.add_pipe(sentencizer)
 
-    def getSegments(self, delim):
-        # Split segments on the segment delimiter
-        # TODO need to remove the section number and title
+class Pipeline(object):
 
-        # TODO Each item begins with a section delimiter, therefore the
-        # firs segment is an empty string. Need to fix this
-        rawSegs = self.raw.split(sep=delim)
-        return rawSegs
+    def __init__(self, dataPath):
+        self.trainPath = Path(dataPath).joinpath('train', 'train')
+        self.testPath = Path(dataPath).joinpath('train', 'test')
+        self.devPath = Path(dataPath).joinpath('train', 'dev')
+        self.expPath = Path(dataPath).joinpath('test')
+        for name, proc in nlp.pipeline:
+            print(name, proc)
 
+    def getFilePaths(self, dataPath):
+        return [x for x in dataPath.glob('**/*') if x.is_file()]
 
+    def processDirectory(self, dataPath):
+        files = self.getFilePaths(dataPath)
+        self.docs = [nlp(file.read_text(encoding='utf-8')) for file in files]
 
-class Segment(object):
+def main(dataPath):
+    pipe = Pipeline(dataPath=dataPath)
 
-    def __init__(self, rawText):
-        self.raw = rawText
+    pipe.processDirectory(pipe.expPath)
+
+if __name__ == '__main__':
+    mainPath = '/Users/bmmidei/Projects/SliceCast/data'
+
+    main(dataPath=mainPath)
