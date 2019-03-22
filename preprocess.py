cfaec03ac3561a042a07cb65135a4b74e8330c7a
Brian Midei <brian.midei@gmail.com>
2019-03-22 00:50:39 -0400

    Added tfrecord generation to preprocessing.
    Base functionality is there.
    
    Need to refine. Add dynamic tfrecord naming.
    Check speed - might need to optimize for efficiency.

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,8 @@
 from __future__ import unicode_literals
-#from spacyOps import Collection
+from spacyOps import customLabeler
 from pathlib import Path
 import spacy
+import tensorflow as tf
 import time
 nlp = spacy.load('en')
 nlp.remove_pipe('ner')
@@ -11,10 +12,12 @@ nlp.remove_pipe('parser')
 sentencizer = nlp.create_pipe('sentencizer')
 #nlp.add_pipe(tokenizer)
 nlp.add_pipe(sentencizer)
+nlp.add_pipe(customLabeler)
 
 class Pipeline(object):
 
     def __init__(self, dataPath):
+        self.mainPath = Path(dataPath)
         self.trainPath = Path(dataPath).joinpath('train', 'train')
         self.testPath = Path(dataPath).joinpath('train', 'test')
         self.devPath = Path(dataPath).joinpath('train', 'dev')
@@ -22,12 +25,50 @@ class Pipeline(object):
         for name, proc in nlp.pipeline:
             print(name, proc)
 
-    def getFilePaths(self, dataPath):
-        return [x for x in dataPath.glob('**/*') if x.is_file()]
-
     def processDirectory(self, dataPath):
         files = self.getFilePaths(dataPath)
         self.docs = [nlp(file.read_text(encoding='utf-8')) for file in files]
+
+    def getFilePaths(self, dataPath):
+        return [x for x in dataPath.glob('**/*') if x.is_file()]
+
+    def genSingleRecord(self, fpath, doc, lemma=True):
+
+        # Helper functions for creating tfrecords
+        def _bytes_feature(value):
+            encoded = [x.encode('utf8') for x in value]
+            return tf.train.Feature(bytes_list=tf.train.BytesList(value=encoded))
+        def _int64_feature(value):
+            return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+
+        if not fpath.exists():
+            try:
+                # Initialize tensorflow writer
+                #writer = tf.data.experimental.TFRecordWriter(str(fpath)) # tf 2.x version
+                writer = tf.io.TFRecordWriter(str(fpath)) # tf 1.x version
+
+                # Create features of example from spacy Document object
+                feature = {}
+                for i, sent in enumerate(doc.user_data['sents']):
+                    if lemma:
+                        feature['sent_'+str(i)] = _bytes_feature([x.lemma_ for x in sent])
+                    else:
+                        feature['sent_'+str(i)] = _bytes_feature([x.lower_ for x in sent])
+                feature['labels'] = _int64_feature(doc.user_data['labels'])
+
+                # Construct the Example proto object
+                example = tf.train.Example(features=tf.train.Features(feature=feature))
+                
+                # Serialize the example to a string
+                serialized = example.SerializeToString()
+
+                # write the serialized object to the disk
+                writer.write(serialized)
+                writer.close()
+
+            except Exception as e:
+                print('Error creating tf record for ' + str(fpath))
+                print(e)
 
 def main(dataPath):
     pipe = Pipeline(dataPath=dataPath)
