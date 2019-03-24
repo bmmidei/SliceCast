055b687cafcf2ac3873b9818e696855d57f760d5
Brian Midei <brian.midei@gmail.com>
2019-03-24 15:58:46 -0400

    Custom Labeler for SliceCast specific nlp ops

---
+++ spacyOps.py
@@ -0,0 +1,42 @@
+import re
+import spacy
+
+
+def createSpacyPipe():
+    # Load baseline Spacy pipeline
+    nlp = spacy.load('en')
+
+    # Remove unneeded elements
+    nlp.remove_pipe('ner')
+    nlp.remove_pipe('tagger')
+    nlp.remove_pipe('parser')
+
+    # Add sentancizer and custom labeler
+    sentencizer = nlp.create_pipe('sentencizer')
+    nlp.add_pipe(sentencizer)
+    nlp.add_pipe(customLabeler)
+
+    return nlp
+
+def customLabeler(doc):
+    sents = [sent.text.strip() for sent in doc.sents] # Remove whitespace
+    sents = [sent for sent in sents if sent] # Remove empty strings
+
+    numSents = len(sents)
+    labels = [0] * numSents
+
+    for i, sent in enumerate(sents):
+        # Search for the split line and label it -1
+        if re.search('========,[0-9]+,.+\.', sent):
+            labels[i] = -1
+            labels[i+1] = 1
+
+    # Remove split lines and corresponding labels
+    sents = [x for i,x in enumerate(sents) if labels[i]!=-1]
+    labels = [x for x in labels if x!=-1]
+
+    data = {'sents':sents,
+            'labels':labels}
+    doc.user_data = data
+
+    return doc
