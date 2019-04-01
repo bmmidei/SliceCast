c55e53da98a8a9c12e236b2b16e91fef038d7459
Brian Midei <brian.midei@gmail.com>
2019-03-31 21:25:16 -0400

    Working version of network

--- netAttempt.py
+++ netAttempt.py
@@ -1,3 +1,4 @@
+#%%
 # Small Change
 import tensorflow as tf
 import tensorflow_hub as hub
@@ -7,104 +8,101 @@ from tensorflow.keras.layers import Layer, Dense, Input, Lambda, \
     Bidirectional, LSTM, Activation, TimeDistributed, Concatenate
 from tensorflow.keras import Model, Sequential
 from tensorflow.keras import backend as K
-
+import numpy as np 
 from netUtils import docGen
 import h5py
 from tensorflow.keras.utils import to_categorical
 
+def UniversalEmbedding(input):
+    # Explicitly cast the input tensor to strings
+    x_str = tf.cast(input, tf.string)
+
+    # Embed each sentence of the input tensor
+    embedTens = tf.map_fn(lambda x: embed(x), x_str, dtype=tf.float32)
+
+    return embedTens
+
+
 def defineModel():
     # Batch shape not included in Input shape
-    encoderIn = Input(shape=[maxlen], dtype=tf.string, name='encoderIn')
-    encoderOut = Lambda(UniversalEmbedding, output_shape=(None, maxlen, 512), name='encoderOut')(encoderInputs)
-    merged = Bidirectional(LSTM(128, return_sequences=True), input_shape=(maxlen, 512))(encoded)
-    preds = Dense(2)(merged)
-    preds = Activation('softmax')(preds)
+    encoderIn = Input(shape=[None], dtype=tf.string, name='encoderIn')
+    encoderOut = Lambda(UniversalEmbedding, output_shape=(None, None, 512), name='encoderOut')(encoderIn)
+    lstm1 = Bidirectional(LSTM(128, return_sequences=True), name='lstm_1', input_shape=(None, 512))(encoderOut)
+    lstm2 = Bidirectional(LSTM(128, return_sequences=True), name='lstm_2', input_shape=(None, 256))(lstm1)
+    preds = Dense(1)(lstm2)
+    #preds = Activation('softmax')(preds)
 
-    model = Model(inputs=[encoderInputs], outputs=[preds, encoderIn, encoderOut])
-    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+    model = Model(inputs=[encoderIn], outputs=[preds])
+    model.compile(loss='binary_crossentropy', optimizer='adam')
     model.summary()
 
     return model
- 
-g1 = tf.Graph()
-with g1.as_default():
+#%%
+
+with tf.Graph().as_default():
     url = "https://tfhub.dev/google/universal-sentence-encoder-large/3"
     embed = hub.Module(url)
-    embeddings = embed([
-        "The quick brown fox jumps over the lazy dog.",
-        "I am a sentence for which I would like to get its embedding"])
-    print(embeddings)
+   
+    files = ['/Users/bmmidei/Projects/SliceCast/data/hdf5/0.hdf5',
+            '/Users/bmmidei/Projects/SliceCast/data/hdf5/1.hdf5',
+            '/Users/bmmidei/Projects/SliceCast/data/hdf5/2.hdf5',
+            '/Users/bmmidei/Projects/SliceCast/data/hdf5/3.hdf5']
 
-
-    maxlen = None # we might need to set a max document length and pad
-    def UniversalEmbedding(input):
-        x_str = tf.cast(input, tf.string)
-        print(x_str.shape)
-        embedTens = tf.map_fn(lambda x: embed(x), x_str, dtype=tf.float32)
-        # Flatten because the universal sentence encoder takes a 1 dimensional input
-        #embedFlat = embed(tf.reshape(tf.cast(x, tf.string), shape=[-1]))
-        print(embedTens.shape)
-        # Reshape back into the correct dimensions
-        # return tf.reshape(embedFlat, [-1, maxlen, 512])
-        print(embedTens.dtype)
-        return embedTens
-    
-    files = ['/Users/bmmidei/Projects/SliceCast/data/hdf5/0.hdf5']
     gen = docGen(files)
     with h5py.File('/Users/bmmidei/Projects/SliceCast/data/hdf5/0.hdf5', 'r') as hf:
         # Iterate through each document within the file
+        x = 0
         for _, grp in hf.items():
             # Extract items and return
             sents = grp['sents'].value
             labels = grp['labels'].value
-            labels = to_categorical(labels)
+            #labels = to_categorical(labels)
             testx, testy =  sents, labels
-            break
-    #files = ['/Users/bmmidei/Projects/SliceCast/data/hdf5/1.hdf5']
-    #testGen = docGen(files)
+            testx = np.expand_dims(testx, axis=0)
+            testy = np.expand_dims(testy, axis=0)
+            x += 1
+            if x>3:
+                break
     with tf.Session() as sess:
         initOp = [tf.global_variables_initializer(), tf.initializers.tables_initializer()]
         sess.run(initOp)
         model = defineModel()
-        model.fit_generator(gen, steps_per_epoch=20, verbose=2)
-        print('moving on')
-        testx, testy
-        print('generating predictions')
-        print(model.predict(testx))
-        print(testy)
-'''
-class ElmoEmbeddingLayer(Layer):
-    def __init__(self, **kwargs):
-        self.dimensions = 1024
-        self.trainable = True
-        super(ElmoEmbeddingLayer, self).__init__(**kwargs)
-    def build(self, input_shape):
-        self.elmo = hub.Module('https://tfhub.dev/google/elmo/2', trainable=self.trainable, name="{}_module".format(self.name))
-        self.trainable_weights += K.tf.trainable_variables(scope="^{}_module/.*".format(self.name))
-        super(ElmoEmbeddingLayer, self).build(input_shape)
-    def call(self, x, mask=None):
-        elmo_embds = self.elmo(K.squeeze(K.cast(x, tf.string), axis=1), signature="default", as_dict=True)
-        lstm1_embd = elmo_embds['lstm_outputs1'] #?xTXD
-        lstm2_embd = elmo_embds['lstm_outputs2'] #?xTXD
-        w_embd = tf.identity(elmo_embds['elmo'], name='elmo_word_embd') #?xTXD
-        #taking index of last word in each sentence
-        idx = elmo_embds['sequence_len']-1
-        batch_idx = tf.stack([tf.range(0,tf.size(idx),1),idx],axis=1)
-        # Concatenate first of backward with last of forward to get sentence embeddings
-        dim = lstm1_embd.get_shape().as_list()[-1]
-        sen_embd_1 = tf.concat([lstm1_embd[:,0,int(dim/2):],
-        tf.gather_nd(lstm1_embd[:,:,:int(dim/2)],batch_idx)], axis=-1) #[batch,dim]
-        sen_embd_2 = tf.concat([lstm2_embd[:,0,int(dim/2):],
-        tf.gather_nd(lstm2_embd[:,:,:int(dim/2)],batch_idx)], axis=-1) #[batch,dim]
-        sen_embd = tf.concat([tf.expand_dims(sen_embd_1,axis=2),
-        tf.expand_dims(sen_embd_2,axis=2)], axis=2, name='elmo_sen_embd') #[batch,dim,2]
-        print(sen_embd)
-        e_s = tf.keras.layers.Dense(units=1,use_bias=False)(sen_embd) #?xDx1
-        e_s = tf.squeeze(e_s,axis=2)
-        return e_s
+        class_weight = {0: 1.,
+                        1: 50.}
+        model.fit_generator(gen, steps_per_epoch=40, verbose=2, class_weight=class_weight)
 
-    def compute_mask(self, inputs, mask=None):
-            return K.not_equal(inputs, '--PAD--')
-    def compute_output_shape(self, input_shape):
-            return (input_shape[0], self.dimensions)
-'''
+        layer_name = 'encoderOut'
+        #intermediate_layer_model = Model(inputs=model.input,
+        #                                outputs=model.get_layer(layer_name).output)
+        #intermediate_output = intermediate_layer_model.predict(testx)
+        print('generating predictions')
+        preds = model.predict(testx)
+with tf.Graph().as_default():
+    url = "https://tfhub.dev/google/universal-sentence-encoder-large/3"
+    embed = hub.Module(url)
+    embeddings = embed([
+        "WTTE began operations on June 1, 1984 as the first general-entertainment independent station in central Ohio.",
+        "The station was founded by the Commercial Radio Institute, a subsidiary of the Baltimore-based Sinclair Broadcast Group."
+        ])
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        sess.run(tf.tables_initializer())
+
+        testEmbed = sess.run(embeddings)
+#%%
+print(preds)
+print(testy)
+#%%
+print(intermediate_output.shape)
+print(testEmbed.shape)
+#%%
+import numpy as np
+a = intermediate_output[0,0,:]
+b = testEmbed[0,:]
+print(a.shape)
+print(b.shape)
+print(a[:20])
+print(b[:20])
+for i, j in zip(a, b):
+    if abs(i-j)>1e-7:
+        print(i,j)
--- netUtils.py
+++ netUtils.py
@@ -20,7 +20,10 @@ class Generator:
 
                     yield sents, labels
 '''
-def docGen(files):
+def docGen(files, batch_size=4):
+    cnt = 0
+    batchx = []
+    batchy = []
     for f in files:
         # Iterate through each hdf5 file
         with h5py.File(f, 'r') as hf:
@@ -29,6 +32,19 @@ def docGen(files):
                 # Extract items and return
                 sents = grp['sents'].value
                 labels = grp['labels'].value
-                labels = to_categorical(labels)
-                print('yield')
-                yield sents, labels
+                #labels = to_categorical(labels)
+                sents = np.expand_dims(sents, axis=0)
+                labels = np.expand_dims(labels, axis=0)
+                batchx.append(sents.tolist)
+                batchy.append(labels.tolist)
+                cnt+=1
+                if cnt==batch_size:
+                    print('yield')
+                    print(len(batchx))
+                    print(len(batchy))
+                    #batchx = np.array(batchx, dtype='object')
+                    #batchy = np.array(batchy, dtype='object')
+                    yield batchx, batchy
+                    cnt=0
+                    batchx = []
+                    batchy = []
