ea9ff74784a2f8522c77c9f8125a08f0b35571a6
Brian Midei <brian.midei@gmail.com>
2019-03-31 23:33:25 -0400

    Updates to batchify preprocessing

--- preprocess.py
+++ preprocess.py
@@ -9,7 +9,7 @@ import numpy as np
 
 class Pipeline(object):
 
-    def __init__(self, dataPath):
+    def __init__(self, dataPath, ex_per_batch, ex_per_file):
         self.mainPath = Path(dataPath)
         self.trainPath = Path(dataPath).joinpath('train', 'train')
         self.testPath = Path(dataPath).joinpath('train', 'test')
@@ -17,31 +17,53 @@ class Pipeline(object):
         self.expPath = Path(dataPath).joinpath('test')
         self.hdf5Path = Path(dataPath).joinpath('hdf5')
 
+        self.ex_per_batch = ex_per_batch
+        self.ex_per_file = ex_per_file
         self.nlp = createSpacyPipe()
         for name, proc in self.nlp.pipeline:
             print(name, proc)
 
-    def processDirectory(self, dataPath, maxExamples=None):
+    def processDirectory(self, dataPath, max_examples=None):
+        # Get the file paths for every txt file in the directory
         self.getFilePaths(dataPath)
-        docs = []
-        print('There are {} documents in this directory'.format(self.numExamples))
-        print('Processing a subset of size {}...'.format(maxExamples))
+        print('There are {} documents in this directory'.format(self.num_examples))
 
-        if maxExamples:
-            self.numExamples = maxExamples
+        if max_examples:
+            print('Processing a subset of size {}...'.format(max_examples))
+            self.num_examples = max_examples
 
-        for file in self.files[:maxExamples]:
-            try:
-                doc = self.nlp(file.read_text(encoding='utf-8'))
-                docs.append(doc)
-            except Exception as e:
-                print(file)
-                print(e)
-        self.docs = docs
+        # Determine number of batches to process
+        self.num_batches = math.ceil(self.num_examples/self.ex_per_batch)
+
+        # Process files in batches to reduce memory impact
+        for batchIdx in range(self.num_batches):
+            # Initialize docs at beginning of each batch
+            docs = []
+            self.docs = []
+            # Determine start and stop indices in file list for current batch
+            startIdx = batchIdx * self.ex_per_batch
+            if batchIdx-1 == self.num_batches:
+                stopIdx = self.num_examples
+            else:
+                stopIdx = startIdx + self.ex_per_batch
+            
+            # Run spacy nlp on each document
+            for file in self.files[startIdx:stopIdx]:
+                try:
+                    doc = self.nlp(file.read_text(encoding='utf-8'))
+                    docs.append(doc)
+                except Exception as e:
+                    print(file)
+                    print(e)
+
+            self.docs = docs
+            
+            # Generate HDF5s for current batch
+            self._genHDF5s(batchIdx, startIdx, stopIdx)
 
     def getFilePaths(self, dataPath):
         self.files = [x for x in dataPath.glob('**/*') if x.is_file()]
-        self.numExamples = len(self.files)
+        self.num_examples = len(self.files)
 
     @staticmethod
     def genSingleExample(doc):
@@ -49,26 +71,26 @@ class Pipeline(object):
         sents = np.array(doc.user_data['sents'], dtype=object)
         return sents, labels
 
-    def genHDF5s(self, exPerFile=256, prntInterval=10):
+    def _genHDF5s(self, batchIdx, startIdx, stopIdx, prnt_interval=10):
         # Create directory for processed audio files
         if not self.hdf5Path.exists():
             print('Adding the hdf5 data directory')
             self.hdf5Path.mkdir()
 
-        numFiles = math.floor(self.numExamples/exPerFile)
-        print('There are {} examples in the directory'.format(self.numExamples))
-        print('Creating {} hdf5 files...'.format(numFiles))
+        num_files = math.floor(len(self.docs)/self.ex_per_file)
+        print('There are {} examples in batch {}'.format(len(self.docs), batchIdx+1))
+        print('Creating {} hdf5 files...'.format(num_files))
 
-        for i in range(numFiles):
-            fname = str(self.hdf5Path.joinpath(str(i)).with_suffix('.hdf5'))
+        for i in range(num_files):
+            fname = str(self.hdf5Path.joinpath('batch'+str(batchIdx)+'_'+str(i)).with_suffix('.hdf5'))
             with h5py.File(str(fname), 'w') as f:
-                for j in range(exPerFile):
-                    docIdx = i*exPerFile + j
+                for j in range(self.ex_per_file):
+                    docIdx = i*self.ex_per_file + j
                     sents, labels = self.genSingleExample(self.docs[docIdx])
                     group = f.create_group(str(j))
 
                     group.create_dataset(name='sents', data=sents, dtype=h5py.special_dtype(vlen=str))
                     group.create_dataset(name='labels', data=labels)
 
-            if i%prntInterval==0:
-                print('Generated {} files...'.format(i))
+            if i%prnt_interval==0:
+                print('Generated {} files in batch {}...'.format(i+1, batchIdx))
---
+++ runPreprocess.py
@@ -0,0 +1,35 @@
+import argparse
+from preprocess import Pipeline
+from pathlib import Path
+import time
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-dp', '--data_path', action='store', type=str,
+                    help='path to high level data directory')
+
+parser.add_argument('-v', '--verbose', help='modify output verbosity',
+                    action = 'store_true')
+
+parser.add_argument('-m', '--max_examples', action='store', default=100, type=int,
+                    help='maximum number of examples to consider')
+
+parser.add_argument('-n', '--exPerFile', action='store', default=512, type=int,
+                    help='number of examples to store in each HDF5 file')
+
+args = parser.parse_args()
+
+s = time.time()
+try:
+    pipe = Pipeline(dataPath=args.data_path,
+                    ex_per_batch=16,
+                    ex_per_file=args.exPerFile)
+except TypeError:
+    print('You must enter a data path to process with the -dp flag')
+    print('exiting...')
+    raise
+
+pipe.processDirectory(pipe.expPath, max_examples=args.max_examples)
+e = time.time()
+if args.verbose:
+    print('\nProcessing time: {} seconds'.format(e - s))
