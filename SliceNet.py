c3cabf69e2ed56ccf9d55901127c4aedc2252cee
bmmidei <brian.midei@gmail.com>
2019-04-19 04:06:39 +0000

    Added regression or classification option
    Network can now run categorical crossentropy with 3 classes or run
    MSE with a single output for each sentence. Categorical crossentropy
    works with class weights to handle class imbalance.
    
    Batch generator is updated accordingly as well.
    
    Main jupyter notebook setup to work with either option and includes
    some light visualization

--- SliceNet.py
+++ SliceNet.py
@@ -6,16 +6,9 @@ import os, re
 import tensorflow as tf
 print(tf.__version__)
 
-tf.logging.set_verbosity(tf.logging.ERROR)
-
 import tensorflow_hub as hub
 import numpy as np 
 from netUtils import batchGen, getTestSet
-#from tensorflow.keras.layers import Layer, Dense, Input, Lambda, Dropout,\
-#    Bidirectional, LSTM, Activation, TimeDistributed, Concatenate
-#from tensorflow.keras import Model, Sequential
-#from tensorflow.keras.models import model_from_yaml
-#import tensorflow.keras.backend as K
 
 from keras.layers import Layer, Dense, Input, Lambda, Dropout,\
     Bidirectional, LSTM, Activation, TimeDistributed, Concatenate
@@ -31,25 +24,55 @@ print('importing hub')
 url = "https://tfhub.dev/google/universal-sentence-encoder-large/3"
 embed = hub.Module(url)
 
+
+# Credit: https://github.com/keras-team/keras/issues/3653
+def customCatLoss(onehot_labels, logits):
+    """Custom categorical loss function incorporating class weights
+    Args:
+        onehot_labels: onehot encoded labels - shape = [batch x doclength x numclasses]
+        logits: logits from predictions from network
+    Yields:
+        loss: average loss for the mini-batch
+    """
+    class_weights = [1.0, 10.0, 0.2]
+    # computer weights based on onehot labels
+    weights = tf.reduce_sum(class_weights * onehot_labels, axis=-1)
+
+    # compute (unweighted) softmax cross entropy loss
+    unweighted_losses = tf.nn.softmax_cross_entropy_with_logits_v2(labels=onehot_labels, logits=logits)
+
+    # apply the weights, relying on broadcasting of the multiplication
+    weighted_losses = unweighted_losses * weights
+
+    # average to get final loss
+    loss = tf.reduce_mean(weighted_losses)
+    return loss
+
 class SliceNet():
-    def __init__(self, pretrain=False, weights_path=None):
+    def __init__(self, classification=True, pretrain=False, weights_path=None):
         self.pretrain = pretrain # Not implemented
-        
+        self.classification = classification
         if self.pretrain:
             self.weights_path = weights_path # Not implemented
             
         self.model = self._defineModel()
     
     def map_docs(self, x):
+        """Helper function to embed a give list of sentences using
+        the google universal sentence encoder from tfhub"""
         return embed(x, signature='default', as_dict=True)['default']
     
     def UniversalEmbedding(self, x):
-            # Explicitly cast the input tensor to strings
-            x_str = tf.cast(x, tf.string)
+        """Embedding layer that calls the map_docs function for each
+        document in the batch. The result is an encoding for each sentence
+        of each document in the mini-batch"""
+        
+        # Explicitly cast the input tensor to strings
+        x_str = tf.cast(x, tf.string)
 
-            # Embed each sentence of the input tensor
-            embedTens = tf.map_fn(self.map_docs, x_str, dtype=tf.float32)
-            return embedTens
+        # Embed each sentence of the input tensor
+        embedTens = tf.map_fn(self.map_docs, x_str, dtype=tf.float32)
+        return embedTens
      
     def _defineModel(self):
         # Define network structure
@@ -63,22 +86,24 @@ class SliceNet():
         output = Dropout(0.2)(output)
         output = TimeDistributed(Dense(64, activation='relu'))(output)
         output = Dropout(0.2)(output)
-        preds = TimeDistributed(Dense(3, activation='softmax'))(output)
         
-        model = Model(inputs=encoderIn, outputs=preds)
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['categorical_accuracy'])
-        #model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        # Final output is different for classification and regression models
+        if self.classification:
+            preds = TimeDistributed(Dense(3, activation='softmax'))(output)
+            model = Model(inputs=encoderIn, outputs=preds)
+            model.compile(loss=customCatLoss, optimizer='adam', metrics=['categorical_accuracy'])
+        else:
+            preds = TimeDistributed(Dense(1, activation='sigmoid'))(output)
+            model = Model(inputs=encoderIn, outputs=preds)
+            model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mae'])
         return model
 
     def train(self, train_files, val_files, batch_size=16, epochs=3, steps_per_epoch=1000, maxlen=None, save=True):
         
         # Define batch generator
-        trainGen = batchGen(train_files, batch_size, maxlen)
-        valGen = batchGen(val_files, 4, maxlen)
+        trainGen = batchGen(train_files, batch_size, maxlen, classification=self.classification)
+        valGen = batchGen(val_files, 4, maxlen, classification=self.classification)
         
-        # I think these need to sum to 1
-        #weights = [0.3, 2.64, 0.06]
-        weights = None
         self.model.summary()
         
         print('Starting Training')
@@ -98,10 +123,8 @@ class SliceNet():
                                           verbose=1,
                                           validation_data=valGen,
                                           validation_steps=10,
-                                          class_weight=weights,
                                           callbacks=[cb])
             
-            #TODO add some sort of keras callback function
             #consider updating loss function from accuracy to precision recall or MSE
             
             if save:
--- netUtils.py
+++ netUtils.py
@@ -5,7 +5,7 @@ from keras.utils import Sequence, to_categorical
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.utils import shuffle
 
-def batchGen(filenames, batch_size=16, maxlen=None):
+def batchGen(filenames, batch_size=16, maxlen=None, classification=True):
     """
     Generator function for batches of documents and labels
     from a list of HDF5 files
@@ -52,13 +52,17 @@ def batchGen(filenames, batch_size=16, maxlen=None):
 
                     # pad docs and labels to the length of the longest sample in the batch
                     padded_docs = pad_sequences(batch_docs, dtype=object, value=' ')
-                    #padded_labels = pad_sequences(batch_labels, dtype=int, value=0)
-                    padded_labels = pad_sequences(batch_labels, dtype=int, value=2)
-                    padded_labels = to_categorical(padded_labels, num_classes=3, dtype='int32')
-                    #padded_labels = np.expand_dims(padded_labels, axis=-1)
+                    
+                    if classification:
+                        padded_labels = pad_sequences(batch_labels, dtype=int, value=2)
+                        padded_labels = to_categorical(padded_labels, num_classes=3, dtype='int32')
+                    else:
+                        padded_labels = pad_sequences(batch_labels, dtype=int, value=0)
+                        padded_labels = np.expand_dims(padded_labels, axis=-1)
+                        
                     yield(padded_docs, padded_labels)   
 
-def getTestSet(fname, num_samples=16):
+def getTestSet(fname, num_samples=16, classification=True):
     with h5py.File(fname, 'r') as hf:
         # Get a list of all examples in the file 
         groups = [item[1] for item in hf.items()]
@@ -83,73 +87,12 @@ def getTestSet(fname, num_samples=16):
 
         # pad docs and labels to the length of the longest sample in the batch
         padded_docs = pad_sequences(docs, dtype=object, value=' ')
-        #padded_labels = pad_sequences(labels, dtype=int, value=0)
-        #padded_labels = np.expand_dims(padded_labels, axis=-1)
-        padded_labels = pad_sequences(labels, dtype=int, value=2)
-        padded_labels = to_categorical(padded_labels, num_classes=3, dtype='int32')
+        
+        if classification:
+            padded_labels = pad_sequences(labels, dtype=int, value=2)
+            padded_labels = to_categorical(padded_labels, num_classes=3, dtype='int32')
+        else:
+            padded_labels = pad_sequences(labels, dtype=int, value=0)
+            padded_labels = np.expand_dims(padded_labels, axis=-1)
         
         return padded_docs, padded_labels
-
-'''
-def docGen(fname):
-    """
-    Generator function for a single HDF5 file
-    Args:
-        fname: filename for a single HDF5 file
-    Yields:
-        sents, labels: A tuple of sentences and corresponding
-                       labels for a single example.
-    """
-    maxlen=30
-    with h5py.File(fname, 'r') as hf:
-        # Get a list of all examples in the file 
-        groups = [item[1] for item in hf.items()]
-
-        # Get lists of all sentences and all labels 
-        docs = [grp['sents'][()] for grp in groups]
-        docs = [docs.tolist() for docs in docs]
-        labels = np.array([grp['labels'][()] for grp in groups])
-
-        docs_mod = [x for x in docs if len(x)<maxlen]
-        labels_mod = [x for x in labels if len(x)<maxlen]
-        padded_docs = pad_sequences(docs_mod, maxlen=maxlen, dtype=object, value=' ')
-        padded_labels = pad_sequences(labels_mod, maxlen=maxlen, dtype=int, value=0)
-        print(type(padded_labels))
-        #padded_labels = np.expand_dims(padded_labels, axis=-1)
-
-
-    # Iterate through all sentences and corresponding labels
-    # and yield one at a time
-    for doc, label in zip(padded_docs, padded_labels):
-        if len(label)==0:
-            pass
-        else:
-            yield(doc, label)
-
-class BatchGen(object):
-    def __init__(self, filenames, batch_size=1, shuffle=True, repeat=True):
-        # Create dataset from filenames
-        ds = tf.data.Dataset.from_tensor_slices(filenames)
-
-        # Use flat map to apply a transformation to each HDF5 file.
-        # Each file creates a dataset of examples. Then all examples are combined
-        # into a single dataset
-        ds = ds.flat_map(lambda filename: tf.data.Dataset.from_generator(
-                                            generator=docGen,
-                                            output_types=(tf.string, tf.int64),
-                                            output_shapes=([None,], [None,]),
-                                            args=([filename])))
-        # Apply dataset shuffling, batching, and repeating
-        ds = ds.batch(batch_size=batch_size)
-        ds = ds.prefetch(100)
-        if shuffle:
-            ds = ds.shuffle(buffer_size=16)
-        if repeat:
-            ds = ds.repeat()
-
-        self.ds = ds
-        self.itr = self.ds.make_one_shot_iterator()
-
-    def __next__(self):
-        return self.itr.get_next()
-'''
