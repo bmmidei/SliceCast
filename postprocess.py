fb6a8ec90651bffd1e8cf582448a9f70c4e1edae
bmmidei <brian.midei@gmail.com>
2019-05-03 00:40:07 +0000

    Preliminary postprocessing debugged and now functional

--- postprocess.py
+++ postprocess.py
@@ -1,11 +1,12 @@
 import numpy as np
+import math
 import h5py
 from netUtils import getTestSet
 import keras
 import re
-#import objectpath
+import objectpath
 import json
-#import pke
+import pke
 
 class pkHistory(keras.callbacks.Callback):
     def __init__(self, test_file, num_samples, k=10):
@@ -84,38 +85,43 @@ def pkscore(labels, preds, k=10):
     return 1-(correct/(num_windows))
 
 def getSummaries(doc, labels):
-    summaries = np.empty(shape=labels.count(1), dtype=(str, []))
+    #summaries = np.empty(shape=labels.count(1), dtype=(str, []))
+    summaries = []
     segment = ''
     numSent = 0
     k = 0
     for i, sent in enumerate(doc):
-        if labels[i] and segment != '':
-            extractor = pke.unsupervised.TopicRank(input=segment, language='en')
+        if labels[i]==1 and segment != '':
+            extractor = pke.unsupervised.TopicRank()
+            extractor.load_document(input=segment, language='en')
             extractor.candidate_selection()
             extractor.candidate_weighting()
-            summaries[k] = extractor.get_n_best(n=1)
+            summaries.append(extractor.get_n_best(n=3))
             k = k+1
             segment = sent
             numSent = 1
         else:
             segment = segment + " " + sent
             numSent = numSent + 1
-    extractor = pke.unsupervised.TopicRank(input=segment, language='en')
+    
+    extractor = pke.unsupervised.TopicRank()
+    extractor.load_document(input=segment, language='en')
     extractor.candidate_selection()
     extractor.candidate_weighting()
-    summaries[k] = extractor.get_n_best(n=1)
+    summaries.append(extractor.get_n_best(n=3))
     return summaries
 
 def getTimeStamps(doc, pathJSON, labels):
     keywords = findKeywords(doc, labels)
     timestamps = findTimeStamps(pathJSON, keywords)
-    return timeStamps
+    return timestamps
 
 def findKeywords(doc, labels):
     keywords = []
     for i, sent in enumerate(doc):
-        if labels[i]:
-            keywords.append((re.sub("[^\w]", " ",  sent).split()[0], re.sub("[^\w]", " ",  sent).split()[1]))
+        if labels[i]==1:
+            words = re.sub("[^\w]", " ",  sent).split()[0], re.sub("[^\w]", " ",  sent).split()[1]
+            keywords.append(words)
     return keywords
 
 def findTimeStamps(pathJSON, keywords):
@@ -125,10 +131,10 @@ def findTimeStamps(pathJSON, keywords):
     words = tuple(jsonnn_tree.execute('$..words'))
     k = 0
     timestamps = []
-
     for idx, word in enumerate(words):
-        if word['word'] == keywords[k][0] and idx < len(result_tuple) - 1 and result_tuple[idx + 1]['word'] == keywords[k][1]:
-            timestamps.append(word['startTime'])
+        if keywords[k][0] in word['word'] and keywords[k][1] in words[idx+1]['word']:
+            stamp = math.floor(float(word['startTime'][:-1]))
+            timestamps.append(stamp)
             k = k + 1
             if k == len(keywords):
                 break
